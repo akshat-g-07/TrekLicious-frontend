5a01195943e2e90bb1e298fcd9e8c680befcfd24
Akshat Garg <akshatg805@gmail.com>
2023-07-12 21:45:37 +0530

    Create SeasonTrek.jsx

---
+++ frontend_new/src/pages/landingPage/components/jsx/SeasonTrek.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../css/DetailPart.css";
+import RotatingPart from "./RotatingPart";
+
+const DetailPart = () => {
+  return (
+    <>
+      <video
+        src="videos/vid01.mp4"
+        muted
+        autoPlay
+        loop
+        className="bgVideo"
+      ></video>
+      <div className="detailPart">
+        <div id="title">TrekLicious</div>
+        <div className="rotatingPartHolder">
+          <div>is your much needed</div>
+          <div className="rotatingPart">
+            <RotatingPart />
+          </div>
+        </div>
+        <div className="details">
+          Welcome to Treklicious, the ultimate destination for adventure
+          seekers. Explore a vast selection of exhilarating treks from around
+          the world. Immerse yourself in stunning visuals, find comprehensive
+          details, and plan your next unforgettable journey. Get ready to embark
+          on the trek of a lifetime with Treklicious.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DetailPart;
