649ee84da8377d59d532d5fc5b9c0d1a9598613c
Akshat Garg <akshatg805@gmail.com>
2023-07-13 13:06:25 +0530

    Update DetailPart.jsx

--- frontend_new/src/pages/landingPage/components/jsx/DetailPart.jsx
+++ frontend_new/src/pages/landingPage/components/jsx/DetailPart.jsx
@@ -5,21 +5,28 @@ import RotatingPart from "./RotatingPart";
 const DetailPart = () => {
   return (
     <>
-      <div id="title" style={{ marginTop: "10%" }}>
-        TrekLicious
-      </div>
-      <div className="rotatingPartHolder">
-        <div>is your much needed</div>
-        <div className="rotatingPart">
-          <RotatingPart />
+      <video
+        src="videos/vid01.mp4"
+        muted
+        autoPlay
+        loop
+        className="bgVideo"
+      ></video>
+      <div className="detailPart">
+        <div id="title">TrekLicious</div>
+        <div className="rotatingPartHolder">
+          <div>is your much needed</div>
+          <div className="rotatingPart">
+            <RotatingPart />
+          </div>
         </div>
-      </div>
-      <div className="details">
-        Welcome to Treklicious, the ultimate destination for adventure seekers.
-        Explore a vast selection of exhilarating treks from around the world.
-        Immerse yourself in stunning visuals, find comprehensive details, and
-        plan your next unforgettable journey. Get ready to embark on the trek of
-        a lifetime with Treklicious.
+        <div className="details">
+          Welcome to Treklicious, the ultimate destination for adventure
+          seekers. Explore a vast selection of exhilarating treks from around
+          the world. Immerse yourself in stunning visuals, find comprehensive
+          details, and plan your next unforgettable journey. Get ready to embark
+          on the trek of a lifetime with Treklicious.
+        </div>
       </div>
     </>
   );
