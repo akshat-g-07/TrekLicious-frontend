0f88207be624c187c2a4f1a00a7c6640b4880b04
Akshat Garg <akshatg805@gmail.com>
2023-07-19 23:22:11 +0530

    24

--- frontend_new/src/pages/landingPage/components/jsx/DetailPart.jsx
+++ frontend_new/src/pages/landingPage/components/jsx/DetailPart.jsx
@@ -1,41 +1,35 @@
-import React from "react";
-import "./DetailPart.css";
-import RotatingPart from "./RotatingPart";
+import React from 'react'
+import '../css/DetailPart.css'
+import '../css/DetailPartMedia.css'
+
+import RotatingPart from "./RotatingPart"
 
 const DetailPart = () => {
-  return (
-    <>
-      <video
-        src="videos/vid01.mp4"
-        muted
-        autoPlay
-        loop
-        className="bgVideo"
-      ></video>
-      <div className="detailPart">
-        <div id="title" style={{ marginTop: "10%" }}>
-          TrekLicious
-        </div>
-        <div className="rotatingPartHolder">
-          <div>is your much needed</div>
-          <div className="rotatingPart">
-            <RotatingPart />
-          </div>
-        </div>
-        <div className="details">
-          Welcome to Treklicious, the ultimate destination for adventure
-          seekers. Explore a vast selection of exhilarating treks from around
-          the world. Immerse yourself in stunning visuals, find comprehensive
-          details, and plan your next unforgettable journey. Get ready to embark
-          on the trek of a lifetime with Treklicious.
-        </div>
-        <div className="buttonHolder">
-          <button className="button">Sign Up</button>
-          <button className="button">Log In</button>
-        </div>
-      </div>
-    </>
-  );
-};
+    const description = ["Adventure", "Escape", "Quest", "Getaway", "Odyssey"]
 
-export default DetailPart;
+    return (
+        <>
+            <video src="videos/vid01.mp4" muted autoPlay loop className='bgVideo'></video>
+            <div className='detailPart'>
+                <div id="title" style={{ marginTop: "10%", textShadow: "1px 1px 12.5px black" }}>
+                    TrekLicious
+                </div>
+                <div className='rotatingPartHolder'>
+                    <div>is your much needed</div>
+                    <div className='rotatingPart'>
+                        <RotatingPart descriptions={description} />
+                    </div>
+                </div>
+                <div className='details'>
+                    Welcome to Treklicious, the ultimate destination for adventure seekers. Explore a vast selection of exhilarating treks from around the world. Immerse yourself in stunning visuals, find comprehensive details, and plan your next unforgettable journey. Get ready to embark on the trek of a lifetime with Treklicious.
+                </div>
+                <div className="buttonHolder">
+                    <button className="button">Sign Up</button>
+                    <button className="button">Log In</button>
+                </div >
+            </div >
+        </>
+    )
+}
+
+export default DetailPart
