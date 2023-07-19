57277c7722e7a0bf881cc6291920656dfc2e127c
Akshat Garg <akshatg805@gmail.com>
2023-07-19 23:22:40 +0530

    25

--- frontend_new/src/pages/landingPage/components/jsx/RotatingPart.jsx
+++ frontend_new/src/pages/landingPage/components/jsx/RotatingPart.jsx
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
-const RotatingPart = () => {
-  const descriptions = ["Adventure", "Escape", "Quest", "Getaway", "Odyssey"];
+const RotatingPart = ({ descriptions }) => {
 
-  const [ShowId, setShowId] = useState(0);
+    const [ShowId, setShowId] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      ShowId === descriptions.length - 1 ? setShowId(0) : setShowId(ShowId + 1);
-    }, 2495);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            ShowId === descriptions.length - 1 ? setShowId(0) : setShowId(ShowId + 1);
+        }, 2475);
 
-    return () => clearInterval(interval);
-  }, [ShowId]);
+        return () => clearInterval(interval);
+    }, [ShowId]);
 
-  const animation = {
-    x: ["0%"],
-    y: ["-100%", "0%", "0%", "100%"],
-    scale: [1],
-    transition: {
-      duration: 2.5,
-      times: [0, 0.25, 0.75, 1],
-      repeat: Infinity,
-    },
-  };
+    const animation = {
+        x: ["0%"],
+        y: ["-100%", "0%", "0%", "100%"],
+        scale: [1],
+        transition: {
+            duration: 2.5,
+            times: [0, 0.25, 0.75, 1],
+            repeat: Infinity,
+        },
+    };
 
-  return <motion.div animate={animation}>{descriptions[ShowId]}</motion.div>;
-};
+    return (
+        <motion.div animate={animation} id="rotatingDiv">
+            {descriptions[ShowId]}
+        </motion.div>
+    )
+}
 
-export default RotatingPart;
+export default RotatingPart
