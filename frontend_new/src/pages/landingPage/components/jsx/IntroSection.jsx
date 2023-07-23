f1aa01b99729bd1c3d6afe9b007126333d5bf134
Akshat Garg <akshatg805@gmail.com>
2023-07-23 23:38:07 +0530

    32 IntroSection.jsx

--- frontend_new/src/pages/landingPage/components/jsx/IntroSection.jsx
+++ frontend_new/src/pages/landingPage/components/jsx/IntroSection.jsx
@@ -1,49 +1,51 @@
-import React from "react";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import "./IntroSection.css";
-import "./IntroSectionMedia.css";
-import DetailPart from "./DetailPart";
+import React from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import "../css/IntroSection.css"
+import "../css/IntroSectionMedia.css"
+import DetailPart from './DetailPart'
+import SeasonTrek from './SeasonTrek'
+import StateTrek from './StateTrek'
 
 const IntroSection = () => {
   return (
-    <div className="introSection">
-      <Parallax
-        pages={2}
-        style={{ top: "0", left: "0" }}
-        className="introParallax"
-      >
+    <div className='introSection'>
+      <Parallax pages={5} style={{ top: '0', left: '0' }} className='introParallax'>
         <ParallaxLayer offset={0} speed={0.15}>
-          <div className="introParallaxLayer" id="img01"></div>
+          <div className='introParallaxLayer' id="img01"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.75}>
-          <div className="introParallaxLayer" id="img02"></div>
+          <div className='introParallaxLayer' id="img02"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.25}>
-          <div className="introParallaxLayer" id="img03"></div>
+          <div className='introParallaxLayer' id="img03"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.75}>
-          <div className="introParallaxLayer" id="img04"></div>
+          <div className='introParallaxLayer' id="img04"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.55}>
-          <div className="introParallaxLayer" id="img05"></div>
+          <div className='introParallaxLayer' id="img05"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.95}>
-          <div className="introParallaxLayer" id="img06"></div>
+          <div className='introParallaxLayer' id="img06"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.15}>
-          <div className="introParallaxLayer" id="title">
-            <p>TrekLicious</p>
-          </div>
+          <div className='introParallaxLayer' id="title"><p>TrekLicious</p></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={1}>
-          <div className="introParallaxLayer" id="img07"></div>
+          <div className='introParallaxLayer' id="img07"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0.25}>
           <DetailPart />
         </ParallaxLayer>
+        <ParallaxLayer sticky={{ start: 2, end: 3 }} speed={0.25} style={{ backgroundColor: "#A1D2CE" }}>
+          <SeasonTrek />
+        </ParallaxLayer>
+        <ParallaxLayer offset={4} speed={0.25}>
+          <StateTrek />
+        </ParallaxLayer>
       </Parallax>
     </div>
-  );
-};
+  )
+}
 
-export default IntroSection;
+export default IntroSection
