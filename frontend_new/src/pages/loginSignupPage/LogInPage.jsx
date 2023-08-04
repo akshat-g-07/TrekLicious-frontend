b7dfcea609402307d3828567e188ad00a74f7ddc
Akshat Garg <akshatg805@gmail.com>
2023-08-04 21:37:44 +0530

    51 LogInPage.jsx

---
+++ frontend_new/src/pages/loginSignupPage/LogInPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LogInPageBackground from "./components/jsx/LogInPageBackground"
+
+const LogInPage = () => {
+    return (
+        <>
+            <LogInPageBackground />
+        </>
+    )
+}
+
+export default LogInPage
