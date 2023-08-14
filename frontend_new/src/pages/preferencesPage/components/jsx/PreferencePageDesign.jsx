d0e67e6a9b52478258c3d5f8872146f74bb2c402
Akshat Garg <akshatg805@gmail.com>
2023-08-14 23:39:10 +0530

    Create PreferencePageDesign.jsx

---
+++ frontend_new/src/pages/preferencesPage/components/jsx/PreferencePageDesign.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StepperPart from "./StepperPart";
+
+const PreferencePageDesign = () => {
+  return (
+    <div className="formHolderBG">
+      <StepperPart />
+    </div>
+  );
+};
+
+export default PreferencePageDesign;
