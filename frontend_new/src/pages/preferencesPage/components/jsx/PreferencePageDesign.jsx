dc15e859b09ac14f16590ec24761cc5154d2482a
Akshat Garg <akshatg805@gmail.com>
2023-08-15 12:46:24 +0530

    Update PreferencePageDesign.jsx

--- frontend_new/src/pages/preferencesPage/components/jsx/PreferencePageDesign.jsx
+++ frontend_new/src/pages/preferencesPage/components/jsx/PreferencePageDesign.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import StepperPart from "./StepperPart";
+import PreferenceFormPart from "./PreferenceFormPart";
 
 const PreferencePageDesign = () => {
   return (
     <div className="formHolderBG">
       <StepperPart />
+      <PreferenceFormPart />
     </div>
   );
 };
