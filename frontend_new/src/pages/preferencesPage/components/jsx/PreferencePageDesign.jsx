3482fe5a84fd75edb635cdace98d1edc1941c826
Akshat Garg <akshatg805@gmail.com>
2023-08-21 10:00:47 +0530

    Update PreferencePageDesign.jsx

--- frontend_new/src/pages/preferencesPage/components/jsx/PreferencePageDesign.jsx
+++ frontend_new/src/pages/preferencesPage/components/jsx/PreferencePageDesign.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import StepperPart from "./StepperPart";
 import PreferenceFormPart from "./PreferenceFormPart";
 import ButtonDesign from "./ButtonDesign";
 
 const PreferencePageDesign = () => {
+  const [direction, setDirection] = useState();
+  const [divIndex, setDivIndex] = useState(0);
+
+  const [prefState, setPrefState] = useState("NA");
+  const [prefSeason, setPrefSeason] = useState("NA");
+  const [prefDifficulty, setPrefDifficulty] = useState("NA");
+
   return (
     <div className="formHolderBG">
-      <StepperPart />
-      <PreferenceFormPart />
-      <ButtonDesign />
+      <StepperPart divIndexValue={divIndex} />
+      <PreferenceFormPart
+        divIndexValue={divIndex}
+        directionValue={direction}
+        prefStateValue={prefState}
+        prefSeasonValue={prefSeason}
+        prefDifficultyValue={prefDifficulty}
+      />
+      <ButtonDesign
+        divIndexValue={divIndex}
+        setDivIndexValue={setDivIndex}
+        setDirectionValue={setDirection}
+        setPrefStateValue={setPrefState}
+        setPrefSeasonValue={setPrefSeason}
+        setPrefDifficultyValue={setPrefDifficulty}
+      />
     </div>
   );
 };
