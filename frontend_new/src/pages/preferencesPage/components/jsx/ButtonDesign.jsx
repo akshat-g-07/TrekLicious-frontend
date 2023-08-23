750e7e934a5e54f5625b3deb61ff0f37255d686b
Akshat Garg <akshatg805@gmail.com>
2023-08-23 23:08:01 +0530

    86 ButtonDesign.jsx

--- frontend_new/src/pages/preferencesPage/components/jsx/ButtonDesign.jsx
+++ frontend_new/src/pages/preferencesPage/components/jsx/ButtonDesign.jsx
@@ -1,79 +1,37 @@
-import React from "react";
-import Button from "@mui/material/Button";
-import "../css/ButtonDesign.css";
+import React from 'react'
+import Button from '@mui/material/Button';
+import "../css/ButtonDesign.css"
 
-const ButtonDesign = ({
-  divIndexValue,
-  setDivIndexValue,
-  setDirectionValue,
-  setPrefStateValue,
-  setPrefSeasonValue,
-  setPrefDifficultyValue,
-}) => {
-  const backButtonClicked = () => {
-    setDivIndexValue((index) => {
-      return (index -= 1);
-    });
-    setDirectionValue(-1);
-  };
+const ButtonDesign = ({ divIndexValue, setDivIndexValue, setDirectionValue, setPrefStateValue, setPrefSeasonValue, setPrefDifficultyValue }) => {
 
-  const nextButtonClicked = () => {
-    divIndexValue === 0
-      ? setPrefStateValue(
-          document.querySelector(".selectedOptionSpan")
-            ? document.querySelector(".selectedOptionSpan").innerHTML
-            : "NA"
-        )
-      : setPrefSeasonValue(
-          document.querySelector(".selectedOptionSpan")
-            ? document.querySelector(".selectedOptionSpan").innerHTML
-            : "NA"
-        );
+    const backButtonClicked = () => {
+        setDivIndexValue((index) => { return index -= 1 })
+        setDirectionValue(-1)
+    }
 
-    setDivIndexValue((index) => {
-      return (index += 1);
-    });
-    setDirectionValue(1);
-  };
+    const nextButtonClicked = () => {
+        divIndexValue === 0 ? setPrefStateValue(document.querySelector(".selectedOptionSpan") ? document.querySelector(".selectedOptionSpan").innerHTML === "Other:" ? document.querySelector("#autocompleteValue").value : document.querySelector(".selectedOptionSpan").innerHTML : "NA") : setPrefSeasonValue(document.querySelector(".selectedOptionSpan") ? document.querySelector(".selectedOptionSpan").innerHTML : "NA")
 
-  const finishButtonClicked = () => {
-    setPrefDifficultyValue(
-      document.querySelector(".selectedOptionSpan")
-        ? document.querySelector(".selectedOptionSpan").innerHTML
-        : "NA"
-    );
-    console.log("finished");
-  };
+        setDivIndexValue((index) => { return index += 1 })
+        setDirectionValue(1)
+    }
 
-  return (
-    <>
-      <div
-        style={{
-          width: "100%",
-          marginTop: "20px",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Button
-          variant="outlined"
-          onClick={backButtonClicked}
-          disabled={divIndexValue === 0 ? true : false}
-        >
-          Back
-        </Button>
-        {divIndexValue === 2 ? (
-          <Button variant="contained" onClick={finishButtonClicked}>
-            Finish
-          </Button>
-        ) : (
-          <Button variant="contained" onClick={nextButtonClicked}>
-            Next
-          </Button>
-        )}
-      </div>
-    </>
-  );
-};
+    const finishButtonClicked = () => {
+        setPrefDifficultyValue(document.querySelector(".selectedOptionSpan") ? document.querySelector(".selectedOptionSpan").innerHTML : "NA")
+        console.log("finished")
+    }
 
-export default ButtonDesign;
+    return (
+        <>
+            <div style={{ width: "100%", marginTop: "20px", display: "flex", justifyContent: "space-between" }}>
+                <Button variant="outlined" onClick={backButtonClicked} disabled={divIndexValue === 0 ? true : false}>Back</Button>
+                {divIndexValue === 2 ?
+                    <Button variant="contained" onClick={finishButtonClicked}>Finish</Button>
+                    :
+                    <Button variant="contained" onClick={nextButtonClicked}>Next</Button>}
+            </div>
+        </>
+    )
+}
+
+export default ButtonDesign
