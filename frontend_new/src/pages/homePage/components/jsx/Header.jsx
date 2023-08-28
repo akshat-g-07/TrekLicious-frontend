6743d23a31f337d674390bc4a5dbb3aa444f8172
Akshat Garg <akshatg805@gmail.com>
2023-08-28 23:21:55 +0530

    94 Header.jsx

--- frontend_new/src/pages/homePage/components/jsx/Header.jsx
+++ frontend_new/src/pages/homePage/components/jsx/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Switch from './Switch';
 import ProfileIcon from './ProfileIcon';
+import SearchBox from './SearchBox';
 import "../css/Header.css"
 
 const Header = () => {
@@ -8,16 +9,21 @@ const Header = () => {
         <>
             <header className='headerClass day'>
                 <div className="headerTitle">TrekLicious</div>
-                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "7.5%" }}>
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "25%" }}>
+                    <div style={{ width: "80%", alignItems: "end", overflow: "hidden" }}>
+                        <SearchBox />
+                    </div>
                     <div style={{ overflow: "hidden", cursor: "pointer" }} onClick={() => {
                         document.querySelector(".headerClass").classList.toggle("day")
                         document.querySelector(".switchClass").classList.toggle("day")
+                        document.querySelector(".searchIcon").classList.toggle("day")
                     }}>
                         <Switch />
                     </div>
                     <div style={{ display: "flex", alignItems: "center" }}>
                         <ProfileIcon />
                     </div>
+
                 </div>
             </header>
         </>
