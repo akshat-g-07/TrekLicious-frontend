19b79b606978a59906ea7ae99c5a6af9a64d0947
Akshat Garg <akshatg805@gmail.com>
2023-08-30 23:07:55 +0530

    97 HomePage.jsx

--- frontend_new/src/pages/homePage/HomePage.jsx
+++ frontend_new/src/pages/homePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/jsx/Header';
+import MainDiv from './components/jsx/MainDiv';
 
 const HomePage = () => {
     return (
         <>
             <Header />
+            <MainDiv />
         </>
     )
 }
