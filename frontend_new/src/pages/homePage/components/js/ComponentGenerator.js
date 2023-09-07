f47484e537d9f366aaf6bd547d4cde267e4b3937
Akshat Garg <akshatg805@gmail.com>
2023-09-07 22:28:08 +0530

    113 ComponentGenerator.js

---
+++ frontend_new/src/pages/homePage/components/js/ComponentGenerator.js
@@ -0,0 +1,39 @@
+import { seasonLines, stateLines, difficultyLines } from "./Lines"
+import TrekCategoryGenerator from "../js/TrekCategoryGenerator"
+import Treks from "../../../landingPage/components/Treks.json"
+
+
+const GenerateStateLine = (stateName) => {
+    const index = Math.floor(Math.random() * stateLines.length)
+    const sectionArray = Treks.filter(trek => trek.state === stateName).slice(0, 10);
+    return { "sectionHeadValue": stateLines[index] + stateName, "sectionArrayValue": sectionArray }
+}
+
+const GenerateSeasonLine = (seasonName) => {
+    const index = Math.floor(Math.random() * seasonLines.length)
+    const sectionArray = Treks.filter(trek => trek.season === seasonName).slice(0, 10);
+    return { "sectionHeadValue": seasonLines[index] + seasonName, "sectionArrayValue": sectionArray }
+}
+
+const GenerateDifficultyLine = (difficultyName) => {
+    const index = Math.floor(Math.random() * difficultyLines.length)
+    const sectionArray = Treks.filter(trek => trek.difficulty === difficultyName).slice(0, 10);
+    return { "sectionHeadValue": difficultyLines[index] + difficultyName + " Treks", "sectionArrayValue": sectionArray }
+}
+
+
+const ComponentGenerator = () => {
+    const trekCategory = TrekCategoryGenerator();
+
+    const componentValue = []
+
+    trekCategory.forEach(trek => {
+        return componentValue.push(Object.keys(trek)[0] === "state" ? GenerateStateLine(Object.values(trek)[0])
+            : Object.keys(trek)[0] === "season" ? GenerateSeasonLine(Object.values(trek)[0])
+                : GenerateDifficultyLine(Object.values(trek)[0]))
+    })
+
+    return componentValue;
+}
+
+export default ComponentGenerator;
