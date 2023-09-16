9516949344e07617fc4353e0f745458d9b702aba
Akshat Garg <akshatg805@gmail.com>
2023-09-16 22:47:09 +0530

    128 TrekDetailPage.jsx

---
+++ frontend_new/src/pages/trekDetailShower/TrekDetailPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import TDPHeader from "./components/jsx/TDPHeader"
+import TDPBody from "./components/jsx/TDPBody"
+
+const TrekDetailPage = () => {
+    return (
+        <>
+            <div className='trekDetailPage'>
+                <TDPHeader />
+                <TDPBody />
+
+            </div>
+        </>
+    )
+}
+
+export default TrekDetailPage
