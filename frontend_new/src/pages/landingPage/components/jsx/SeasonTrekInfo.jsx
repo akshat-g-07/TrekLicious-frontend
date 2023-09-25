b7f46881301e17d80a2257e91ac4676f00e47f6a
Akshat Garg <akshatg805@gmail.com>
2023-09-25 22:42:02 +0530

    139 SeasonTrekInfo.jsx

--- frontend_new/src/pages/landingPage/components/jsx/SeasonTrekInfo.jsx
+++ frontend_new/src/pages/landingPage/components/jsx/SeasonTrekInfo.jsx
@@ -1,60 +1,31 @@
-import React from 'react';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import React from "react";
 import Treks from "./Treks.json";
-import TrekParallax from './TrekParallax';
-import './SeasonTrekInfo.css';
-import './SeasonTrekInfoMedia.css';
+import SeasonTrekInfoLayer from "./SeasonTrekInfoLayer";
+import "./SeasonTrekInfo.css";
+import "./SeasonTrekInfoMedia.css";
 
 const SeasonTrekInfo = () => {
-    const genRandomLetter = () => {
-        const letters = ['Small', 'Medium', 'Large'];
-        const randomIndex = Math.floor(Math.random() * letters.length);
-        const randomLetter = letters[randomIndex];
-        return randomLetter;
-    }
+  const SummerTreks = Treks.filter((trek) => trek.season === "Summer");
+  const MonsoonTreks = Treks.filter((trek) => trek.season === "Monsoon");
+  const WinterTreks = Treks.filter((trek) => trek.season === "Winter");
+  const traversalArray = [SummerTreks, MonsoonTreks, WinterTreks];
 
-    const SummerTreks = Treks.filter(trek => trek.season === "Summer");
-    const MonsoonTreks = Treks.filter(trek => trek.season === "Monsoon");
-    const WinterTreks = Treks.filter(trek => trek.season === "Winter");
+  return (
+    <>
+      <div className="introPageSeasonHolder">
+        {traversalArray.map((trek, index) => {
+          return (
+            <SeasonTrekInfoLayer
+              key={index}
+              trekArray={trek}
+              rotateVal={index === 1 ? "rotateY(180deg)" : "rotateY(0deg)"}
+              mid={index === 1}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
-    const parallaxlayerWidth = window.innerWidth < 768 ? "115px" : "500px"
-    const parallaxlayerHeight = window.innerWidth < 768 && "100px"
-    const parallaxHeight = window.innerWidth < 768 ? "100px" : "150px"
-
-    return (
-        <>
-            <div className='introPageSeasonHolder'>
-                <div className="introPageTrekHolder">
-                    <Parallax pages={Math.ceil(SummerTreks.length / 4)} horizontal style={{ height: parallaxHeight }}>
-                        {SummerTreks.map((trek, index) => {
-                            return <ParallaxLayer key={index} offset={index / 4} speed={window.innerWidth < 1024 ? 0.25 : parseFloat(Math.random().toFixed(2))} className='introPageTrekSeason'
-                                style={{ width: parallaxlayerWidth, height: parallaxlayerHeight }}>
-                                <TrekParallax mid={false} treks={trek} dimension={genRandomLetter()} />
-                            </ParallaxLayer>
-                        })}
-                    </Parallax>
-                </div>
-                <div className="introPageTrekHolder" style={{ transform: "rotateY(-180deg)" }}>
-                    <Parallax pages={Math.ceil(MonsoonTreks.length / 4)} horizontal style={{ height: parallaxHeight }}>
-                        {MonsoonTreks.map((trek, index) => {
-                            return <ParallaxLayer key={index} offset={index / 4} speed={window.innerWidth < 1024 ? 0.25 : parseFloat(Math.random().toFixed(2))} className='introPageTrekSeason' style={{ width: parallaxlayerWidth, height: parallaxlayerHeight }}>
-                                <TrekParallax mid={true} treks={trek} dimension={genRandomLetter()} />
-                            </ParallaxLayer>
-                        })}
-                    </Parallax>
-                </div>
-                <div className="introPageTrekHolder">
-                    <Parallax pages={Math.ceil(WinterTreks.length / 4)} horizontal style={{ height: parallaxHeight }}>
-                        {WinterTreks.map((trek, index) => {
-                            return <ParallaxLayer key={index} offset={index / 4} speed={window.innerWidth < 1024 ? 0.25 : parseFloat(Math.random().toFixed(2))} className='introPageTrekSeason' style={{ width: parallaxlayerWidth, height: parallaxlayerHeight }}>
-                                <TrekParallax mid={false} treks={trek} dimension={genRandomLetter()} />
-                            </ParallaxLayer>
-                        })}
-                    </Parallax>
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default SeasonTrekInfo
+export default SeasonTrekInfo;
