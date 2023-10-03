6b3240d321e2157f97a74680b005cbd3aa58232c
Akshat Garg <akshatg805@gmail.com>
2023-10-03 23:28:26 +0530

    152 RecommendedSection.jsx

--- frontend_new/src/pages/homePage/components/jsx/RecommendedSection.jsx
+++ frontend_new/src/pages/homePage/components/jsx/RecommendedSection.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import GeneralSection from './GeneralSection';
+import React from "react";
+import GeneralSection from "./GeneralSection";
 import Treks from "../../../landingPage/components/Treks.json";
 
-
 const RecommendedSection = () => {
-
-    const Trek = Treks.filter(trek => { return (trek.state === "Uttarakhand" || trek.season === "Summer" || trek.difficulty === "Easy") })
-    const sectionHeadValue = "Recommended For You";
-    const sectionArrayValue = Trek.sort(() => Math.random() - 0.5).slice(0, 10);
-
+  const Trek = Treks.filter((trek) => {
     return (
-        <>
-            <GeneralSection sectionHead={sectionHeadValue} sectionArray={sectionArrayValue} />
-        </>
-    )
-}
+      trek.state === "Uttarakhand" ||
+      trek.season === "Summer" ||
+      trek.difficulty === "Easy"
+    );
+  });
+  const sectionHeadValue = "Recommended For You";
+  const sectionArrayValue = Trek.sort(() => Math.random() - 0.5).slice(0, 10);
 
-export default RecommendedSection
+  return (
+    <>
+      <GeneralSection
+        sectionHead={sectionHeadValue}
+        sectionArray={sectionArrayValue}
+        recommendedTreks={Trek}
+      />
+    </>
+  );
+};
+
+export default RecommendedSection;
