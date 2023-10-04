ee7636dcb5979c8bdf41530c8d8d45426b036634
Akshat Garg <akshatg805@gmail.com>
2023-10-04 20:40:13 +0530

    153 ProfilePage.jsx

--- frontend_new/src/pages/profilePage/ProfilePage.jsx
+++ frontend_new/src/pages/profilePage/ProfilePage.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ProfilePageDesign from "./components/jsx/ProfilePageDesign"
-import SideSection from "./components/jsx/SideSection"
+import React from "react";
+import ProfilePageDesign from "./components/jsx/ProfilePageDesign";
+import SideSection from "./components/jsx/SideSection";
 
 const ProfilePage = () => {
-    return (
-        <>
-            <div style={{ display: "flex", width: "100%", height: "100%" }}>
-                <SideSection />
-                <ProfilePageDesign />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div style={{ display: "flex", width: "100%", height: "100%" }}>
+        <SideSection classSelected={".profileSide"} />
+        <ProfilePageDesign />
+      </div>
+    </>
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
