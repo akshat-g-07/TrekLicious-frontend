7168a077a46307053400a504f69e1bed40e04553
Akshat Garg <akshatg805@gmail.com>
2023-10-06 20:38:39 +0530

    154 SideSection.jsx

--- frontend_new/src/pages/profilePage/components/jsx/SideSection.jsx
+++ frontend_new/src/pages/profilePage/components/jsx/SideSection.jsx
@@ -1,37 +1,52 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import ForwardIcon from '@mui/icons-material/Forward';
-import "../css/SideSection.css"
+import React, { useEffect } from "react";
+import Button from "@mui/material/Button";
+import ForwardIcon from "@mui/icons-material/Forward";
+import "../css/SideSection.css";
+import { Link } from "react-router-dom";
 
-const SideSection = () => {
-    return (
-        <>
-            <Button variant="outlined" sx={{ position: "fixed", top: "5%", left: "10%" }} startIcon={<ForwardIcon sx={{ transform: "rotateZ(180deg)" }} />}>Back</Button>
-            <div className="sideSectionHolder">
-                <div className="profileSide sideSelected" onClick={() => {
-                    document.querySelector(".favoriteSide").classList.remove("sideSelected")
-                    document.querySelector(".preferenceSide").classList.remove("sideSelected")
-                    document.querySelector(".profileSide").classList.add("sideSelected")
-                }}>
-                    My Profile
-                </div>
-                <div className="favoriteSide" onClick={() => {
-                    document.querySelector(".profileSide").classList.remove("sideSelected")
-                    document.querySelector(".preferenceSide").classList.remove("sideSelected")
-                    document.querySelector(".favoriteSide").classList.add("sideSelected")
-                }}>
-                    My Favorites
-                </div>
-                <div className="preferenceSide" onClick={() => {
-                    document.querySelector(".profileSide").classList.remove("sideSelected")
-                    document.querySelector(".favoriteSide").classList.remove("sideSelected")
-                    document.querySelector(".preferenceSide").classList.add("sideSelected")
-                }}>
-                    My Preferences
-                </div>
-            </div>
-        </>
-    )
-}
+const SideSection = ({ classSelected }) => {
+  useEffect(() => {
+    document.querySelector(classSelected).classList.add("sideSelected");
+  }, []);
+  return (
+    <>
+      <Link to="/home">
+        <Button
+          variant="outlined"
+          sx={{ position: "fixed", top: "5%", left: "10%" }}
+          startIcon={<ForwardIcon sx={{ transform: "rotateZ(180deg)" }} />}
+        >
+          Home
+        </Button>
+      </Link>
+      <div className="sideSectionHolder">
+        <div className="profileSide">
+          <Link
+            to="/my-profile"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            My Profile
+          </Link>
+        </div>
+        <div className="favoriteSide">
+          <Link
+            to="/my-favorites"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            My Favorites
+          </Link>
+        </div>
+        <div className="preferenceSide">
+          <Link
+            to="/my-preferences"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            My Preferences
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SideSection
+export default SideSection;
