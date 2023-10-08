2c8ed85e9b5db3fec4c784a2437b92227d6f0866
Akshat Garg <akshatg805@gmail.com>
2023-10-08 22:20:50 +0530

    156 FavoritePage.jsx

--- frontend_new/src/pages/profilePage/FavoritePage.jsx
+++ frontend_new/src/pages/profilePage/FavoritePage.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import FavoritePageDesign from "./components/jsx/FavoritePageDesign"
-import SideSection from "./components/jsx/SideSection"
+import React from "react";
+import FavoritePageDesign from "./components/jsx/FavoritePageDesign";
+import SideSection from "./components/jsx/SideSection";
 
 const FavoritePage = () => {
-    return (
-        <>
-            <div style={{ display: "flex", width: "100%", height: "100%" }}>
-                <SideSection />
-                <FavoritePageDesign />
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div style={{ display: "flex", width: "100%", height: "100%" }}>
+        <SideSection classSelected={".favoriteSide"} />
+        <FavoritePageDesign />
+      </div>
+    </>
+  );
+};
 
-export default FavoritePage
+export default FavoritePage;
