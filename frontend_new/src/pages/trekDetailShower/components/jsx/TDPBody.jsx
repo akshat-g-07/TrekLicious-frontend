54eaa6873c2551b76b656d7e4f30fcf427df2166
Akshat Garg <akshatg805@gmail.com>
2023-10-10 20:32:00 +0530

    161 TDPBody.jsx

--- frontend_new/src/pages/trekDetailShower/components/jsx/TDPBody.jsx
+++ frontend_new/src/pages/trekDetailShower/components/jsx/TDPBody.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React from "react";
+import FavoritePart from "./FavoritePart";
 import "../css/TDPBody.css";
-import TerrainIcon from '@mui/icons-material/Terrain';
-import MapIcon from '@mui/icons-material/Map';
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import TerrainIcon from "@mui/icons-material/Terrain";
+import MapIcon from "@mui/icons-material/Map";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 
-const TDPBody = () => {
-    return (
-        <>
-            <div className="formHolderBG tdpBody">
-                <div className="tdpTrekName">
-                    Roopkund
-                </div>
-                <div className="tdpDetailsHolder">
-                    <div className='tdpDetail'>
-                        <MapIcon />  State : {"Uttarakhand"}
-                    </div>
-                    <div className='tdpDetail'>
-                        <WbSunnyIcon /> Season : {"Monsoon"}
-                    </div>
-                    <div className='tdpDetail'>
-                        <TerrainIcon /> Difficulty : {"Easy"}
-                    </div>
-                </div>
-                <div className="tdpDescription">
-                    {"some description"}
-                </div>
+const TDPBody = ({
+  trekName,
+  trekState,
+  trekSeason,
+  trekDifficulty,
+  trekDescription,
+}) => {
+  return (
+    <>
+      <div className="formHolderBG tdpBody">
+        <div className="tdpTrekName">
+          {trekName}
+          <FavoritePart />
+        </div>
+        <div className="tdpDetailsHolder">
+          <div className="tdpDetail">
+            <MapIcon /> State:&nbsp;&nbsp;
+            <span style={{ color: "white" }}>{trekState}</span>
+          </div>
+          <div className="tdpDetail">
+            <WbSunnyIcon /> Season:&nbsp;&nbsp;
+            <span style={{ color: "white" }}>{trekSeason}</span>
+          </div>
+          <div className="tdpDetail">
+            <TerrainIcon /> Difficulty:&nbsp;&nbsp;
+            <span style={{ color: "white" }}>{trekDifficulty}</span>
+          </div>
+        </div>
+        <div className="tdpDescription">{trekDescription}</div>
+      </div>
+    </>
+  );
+};
 
-            </div>
-        </>
-    )
-}
-
-export default TDPBody
+export default TDPBody;
